ed79b212cc4178481faf6a0ac87949a083c89f24
yahyaKacem <fuj.tyoli@gmail.com>
2017-05-11 23:38:01 +0100

    testing text print

--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import * as electronPrinter from 'electron-printer2';
 import { app, ipcMain, BrowserWindow } from 'electron';
 
 let mainWindow;
-ipcMain.on('print', (event, args) => {
+ipcMain.on('printFile', (event, args) => {
   console.log(args.file);
   electronPrinter.printFile({
     filename: args.file,
     error:    (err) => console.log(err),
     printer:  electronPrinter.getPrinters()[0].name,
     success:  (jobID) => console.log(`sent to printer with ID: ${jobID}`)
+  });
+});
+ipcMain.on('printText', (event, args) => {
+  electronPrinter.printDirect({
+    type:    'TEXT',
+    data:    args.text,
+    error:   (err) => console.log(err),
+    printer: electronPrinter.getPrinters()[0].name,
+    success: (jobID) => console.log(`sent to printer with ID: ${jobID}`)
   });
 });
 const createWindow = () => {
@@ -23,21 +32,7 @@ const createWindow = () => {
       mainWindow.webContents.send('log', {electronPrinter: electronPrinter});
       mainWindow.webContents.send('showPrinters', {
         printers: electronPrinter.getPrinters(),
-        selectedPaperSize: electronPrinter.getSelectedPaperSize(),
-        defaultPrinterName: electronPrinter.getDefaultPrinterName(),
-        printerDriverOptions: electronPrinter.getPrinterDriverOptions(),
-        supportedJobCommands: electronPrinter.getSupportedJobCommands(),
-        supportedPrintFormats: electronPrinter.getSupportedPrintFormats(),
-        printer: electronPrinter.getPrinter(electronPrinter.getPrinters()[0].name),
-        default: {
-          printer: electronPrinter.default.getPrinter(),
-          printers: electronPrinter.default.getPrinters(),
-          selectedPaperSize: electronPrinter.default.getSelectedPaperSize(),
-          defaultPrinterName: electronPrinter.default.getDefaultPrinterName(),
-          printerDriverOptions: electronPrinter.default.getPrinterDriverOptions(),
-          supportedJobCommands: electronPrinter.default.getSupportedJobCommands(),
-          supportedPrintFormats: electronPrinter.default.getSupportedPrintFormats(),
-        }
+        printer: electronPrinter.getPrinter(electronPrinter.getPrinters()[0].name)
       });
     }
   );
--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 (function(window, console, document) {
   "use strict";
   const
-    printButton = document.querySelector('.btn.btn-primary'),
+    textareaInput   = document.querySelector('#text'),
+    filePrintButton = document.querySelector('.btn.btn-primary.file'),
+    textPrintButton = document.querySelector('.btn.btn-primary.text'),
     ipcRenderer = window['require']('electron').ipcRenderer;
   ipcRenderer.on('showPrinters', (event, args) => {
     console.log(args);
   });
   ipcRenderer.on('log', (event, args) => console.log(args));
-  printButton.addEventListener('click', () => ipcRenderer.send('print', {file: '/Users/admin/Desktop/Print.pdf'}));
+  filePrintButton.addEventListener('click', () => ipcRenderer.send('printFile', {file: '/Users/admin/Desktop/Print.pdf'}));
+  textPrintButton.addEventListener('click', () => ipcRenderer.send('printText', {text: textareaInput.value}));
 }(window, console, document));
