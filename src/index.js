3a609548322763eadc2552368894b8fe36935012
yahyaKacem <fuj.tyoli@gmail.com>
2017-05-12 13:15:19 +0200

    print text throught java

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+import * as path from 'path';
+import * as lodash from 'lodash';
+import { execSync } from 'child_process';
 import * as electronPrinter from 'electron-printer2';
 import { app, ipcMain, BrowserWindow } from 'electron';
 
+const
+  javaLib              = path.join(__dirname, `../assets/libs/regkassen-loyalcraft-1.0.0.jar`),
+  javaPrintTextCommand = [
+    'java',
+    '-cp',
+    `"${javaLib}"`,
+    '"com.loyalcraft.core.Runner"',
+    '-t',
+    '"print"',
+    '-g'
+  ];
 let mainWindow;
 ipcMain.on('printFile', (event, args) => {
   console.log(args.file);
@@ -20,6 +34,17 @@ ipcMain.on('printText', (event, args) => {
     success: (jobID) => console.log(`sent to printer with ID: ${jobID}`)
   });
 });
+ipcMain.on('printTextJava', (event, args) => {
+  console.log('printing through java');
+  execSync(lodash.concat(
+    javaPrintTextCommand,
+    [
+      `"${electronPrinter.getPrinters().filter((printer) => /^EPSON/.test(printer.name))[0].name}"`,
+      '-h',
+      `"${args.text}"`
+    ]
+  ).join(' '));
+});
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -29,10 +54,10 @@ const createWindow = () => {
   mainWindow.webContents.on(
     'did-finish-load',
     () => {
-      mainWindow.webContents.send('log', {electronPrinter: electronPrinter});
+      // mainWindow.webContents.send('log', {electronPrinter: electronPrinter});
       mainWindow.webContents.send('showPrinters', {
         printers: electronPrinter.getPrinters(),
-        printer: electronPrinter.getPrinter(electronPrinter.getPrinters()[0].name)
+        printer: electronPrinter.getPrinter(electronPrinter.getPrinters().filter((printer) => /^EPSON/.test(printer.name))[0].name)
       });
     }
   );
--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
 (function(window, console, document) {
   "use strict";
   const
-    textareaInput   = document.querySelector('#text'),
-    filePrintButton = document.querySelector('.btn.btn-primary.file'),
-    textPrintButton = document.querySelector('.btn.btn-primary.text'),
-    ipcRenderer = window['require']('electron').ipcRenderer;
+    textareaInput       = document.querySelector('#text'),
+    filePrintButton     = document.querySelector('.file'),
+    textPrintButton     = document.querySelector('.text'),
+    textJavaPrintButton = document.querySelector('.text-java'),
+    ipcRenderer         = window['require']('electron').ipcRenderer;
   ipcRenderer.on('showPrinters', (event, args) => {
     console.log(args);
   });
   ipcRenderer.on('log', (event, args) => console.log(args));
   filePrintButton.addEventListener('click', () => ipcRenderer.send('printFile', {file: '/Users/admin/Desktop/Print.pdf'}));
   textPrintButton.addEventListener('click', () => ipcRenderer.send('printText', {text: textareaInput.value}));
+  textJavaPrintButton.addEventListener('click', () => ipcRenderer.send('printTextJava', {text: textareaInput.value}));
 }(window, console, document));
